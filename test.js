82e9ca7464a806057582d66554f7762df54bf557
tomByrer <tomByrer@gmail.com>
2013-12-29 18:02:50 -0700

    No compress in test

--- test.js
+++ test.js
@@ -6,17 +6,17 @@ var uncss = require('./index');
 var html = '<html><head></head><body><h2>Foo Bar</h2></body></html>';
 var html2 = '<html><head></head><body><h2>Foo Bar</h2><div id="extra"></div></body></html>'
 var inlineCSS = 'h2 { font-size:1.5rem } p { font-size:1em } #extra { background: red }';
-var compressed = 'h2{font-size:1.5rem}';
-var ignored = 'h2{font-size:1.5rem}#extra{background:red}';
+var reduced = 'h2 { font-size:1.5rem }';
+var ignored = 'h2 { font-size:1.5rem } #extra { background: red }';
 
 describe('gulp-uncss-task', function() {
     it('should generate a stylesheet with unused CSS removed', function(cb){
         this.timeout(9000);
 
-        var stream = uncss({ html: html, compress: true });
+        var stream = uncss({ html: html });
 
         stream.on('data', function(file) {
-            assert.equal(file.contents.toString(), compressed);
+            assert.equal(file.contents.toString(), reduced);
             cb();
         });
 
@@ -31,8 +31,7 @@ describe('gulp-uncss-task', function() {
         this.timeout(9000);
 
         var stream = uncss({ 
-            html: html2, 
-            compress: true, 
+            html: html2,
             ignore: ['#extra', /test\-[0-9]+/] 
         });
 
