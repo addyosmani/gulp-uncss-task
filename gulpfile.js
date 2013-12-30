9cd33c892c522f45d44a05423862eadd749ef75b
tomByrer <tomByrer@gmail.com>
2013-12-29 18:10:28 -0700

    - compress

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,7 @@ var uncss = require('gulp-uncss-task');
 gulp.task('default', function() {
     gulp.src('bootstrap.css')
         .pipe(uncss({
-            html: ['index.html', 'contact.html', 'about.html'],
-            compress: true
+            html: ['index.html', 'contact.html', 'about.html']
         }))
         .pipe(gulp.dest('dest'));
 });
